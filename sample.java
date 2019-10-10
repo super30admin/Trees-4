0c43a5f355782c18f20a6e80ea5fcc7bd6b21c0d
RamyaRaviprakash <rramya@thoughtworks.com>
2019-10-10 11:45:03 -0700

    lowest common ancestor in BST

--- sample.java
+++ sample.java
@@ -34,3 +34,34 @@ class Solution {
         func(node.right,k);
     }
 }
+
+// Time Complexity : O(n/2)
+// Space Complexity : O(n/2)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+// Approach: A node in a BST is an ancestor of 2 nodes p, q, iff p and q lie on different sides of the tree.
+// if both p,q lies on left then lca is in left side of root and vice versa
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root.val>p.val && root.val>q.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        if(root.val<p.val && root.val<q.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }
+        return root;
+    }
+}
