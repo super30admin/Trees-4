54479dfa882dcdd0027f750952625ae0f28972e7
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2020-01-17 20:36:00 -0800

    Create Problem_2.py

---
+++ Problem_2.py
@@ -0,0 +1,41 @@
+# Runs on Leetcode
+  n is number of nodes
+    # Solution 1
+        # Runtime - O(n)
+        # Memory - O(n) considering the recursive stack
+    # Solution 2
+        # Runtime - O(n)
+        # Memory - O(1)
+
+
+# Solution 1
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode'):
+        if not root:
+            return root
+        
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        
+        return root
+        
+# Solution 2
+        
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode'):
+        if not root:
+            return root
+        
+        while root:
+            if p.val < root.val and q.val < root.val:
+                root = root.left
+
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
+                
+            else:
+                return root
