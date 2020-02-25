6190583a0f0d5cce2ead5e9413739d0c13551da1
Sanjay Vishwakarma <svishwak@andrew.cmu.edu>
2020-02-25 12:22:58 -0800

    Iterative and recursive solution

---
+++ BigN100LowestCommonAncestorIterative.java
@@ -0,0 +1,23 @@
+//Time complexity is O(logn)
+//Space complexity is O(logn)
+//This solution is submitted on leetcode
+
+public class BigN100LowestCommonAncestorIterative {
+	/**
+	 * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+	 * left; TreeNode right; TreeNode(int x) { val = x; } }
+	 */
+	class Solution {
+		public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+			while (root != null) {
+				if (p.val < root.val && q.val < root.val)
+					root = root.left;
+				else if (p.val > root.val && q.val > root.val)
+					root = root.right;
+				else
+					return root;
+			}
+			return null;
+		}
+	}
+}
