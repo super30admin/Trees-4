338d67e37a044ba3d1ee0890be10887fb85d81f2
Arnav Shah <arnav1996@gmail.com>
2020-03-28 21:10:51 -0700

    Added ksmallest element problem

---
+++ ksmallest.java
@@ -0,0 +1,45 @@
+# Time complexity:O(n)
+# Space complexity: 0(1)
+# Did code run successfully on leetcode: yes
+# Any problem you faced: No
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        if(root == null) return 0;
+
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cursor = root;
+        while(cursor != null){
+            stack.push(cursor);
+            cursor = cursor.left;
+        }
+        while(!stack.isEmpty()){
+            cursor = stack.pop();
+            k--;
+            if(k == 0)
+                return cursor.val;
+            if(cursor.right != null){
+                cursor = cursor.right;
+                while(cursor != null){
+                    stack.push(cursor);
+                    cursor = cursor.left;
+                }
+            }
+
+        }
+
+            return 0;
+
+    }
+}
