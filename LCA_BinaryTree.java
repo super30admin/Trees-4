bbc98f9ce6cd20337df47bd3c4aa60d7e6b90858
Aishwarya Sharma <ashfranky@gmail.com>
2020-05-08 19:51:09 -0400

    LCA Binary Tree added

---
+++ LCA_BinaryTree.java
@@ -0,0 +1,26 @@
+//Time complexity: O(N)
+//Space complexity: O(max_depth)
+
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null)
+                return null;
+        
+        if (root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null)
+            return root;
+        // System.out.println(left.val);
+        return left != null ? left: right;
+        
+    }
+}
