0d04460b7d476d3caa5cc7fc1f18718c4f0f79e9
albertcoder <eralbert9191@gmail.com>
2020-06-14 18:03:09 -0700

    find lca in binary tree

---
+++ lcaBinaryTree.java
@@ -0,0 +1,26 @@
+//time complexity O(n)
+//space complexity O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //base
+        if(root == null || root == p || root == q) return root;
+        //logic
+        TreeNode left = lowestCommonAncestor(root.left, p,q);
+        TreeNode right = lowestCommonAncestor(root.right, p,q);
+        if(left != null && right != null) return root;
+        else if(left != null) return left;
+        else if(right != null) return right;
+        else return null;
+
+    }
+}
