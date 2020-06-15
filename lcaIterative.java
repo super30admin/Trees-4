9df1771002f1922baa4ef0dc694c37925a53d440
albertcoder <eralbert9191@gmail.com>
2020-06-14 17:23:52 -0700

    finding lowest common ancestor in a BST using interative approach

---
+++ lcaIterative.java
@@ -0,0 +1,25 @@
+//time complexity O(n) 
+//space complexity O(1)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //base
+        if(root == null) return null;
+        while(root != null){
+            if(root.val < p.val && root.val < q.val) root = root.right;
+            else if(root.val > p.val && root.val > q.val) root = root.left;
+            else return root;
+        }
+        return null;
+    }
+}
