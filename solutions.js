5b277a192407df0c96f9967173b07c20b958a73f
Siyami Avci <siyamiavci@gmail.com>
2020-06-15 18:06:38 -0700

    add solution 2

--- solutions.js
+++ solutions.js
@@ -44,10 +44,28 @@ const kthSmallest = function(root, k) {
 }
 
 
-
-
 // ## Problem2 Lowest Common Ancestor of a Binary Search Tree 
 // (https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/)
+
+//iterative
+const lowestCommonAncestor = function(root, p, q) {
+    while(root !== null) {
+        if(root.val < p.val && root.val < q.val) root = root.right;
+        else if(root.val > p.val && root.val > q.val) root = root.left;
+        else return root;
+    }
+}
+
+
+//recursive
+const lowestCommonAncestor = function(root, p, q) {
+    //base
+    if(root === null) return null;
+    //logic
+    if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right,p,q);
+    else if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left,p,q);
+    else return root;
+};
 
 
 
