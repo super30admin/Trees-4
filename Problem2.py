6fb4162973a3790457d9833e7f16ba5ee95fec35
Shantanu609 <shantanu_shinde_@mail.fresnostate.edu>
2020-07-23 23:02:04 -0700

    Updated TC for problem2

--- Problem2.py
+++ Problem2.py
@@ -1,4 +1,4 @@
-# Time Complexity : O(n) 
+# Time Complexity : O(log n) 
 # Space Complexity :O(h)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
