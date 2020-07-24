4236ff4dd9b70cdd6d937806fbc3aa78db7125a6
akashbir <akashbir07@gmail.com>
2020-07-24 15:26:23 -0400

    Completed

---
+++ LowestCommonAncestorBTree.java
@@ -0,0 +1,17 @@
+// Time complexity: O(N)
+// Space complexity: O(H)
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // base case
+        if(root == null || p.val == root.val || q.val == root.val) return root;
+
+        // logic
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left != null && right != null) return root;
+        else if(left != null) return left;
+        else if(right != null) return right;
+        return null;
+    }
+}
