7f47435bfd177c14e5f3c48996fd4424d62ce33d
Arpit Sharma <arpit317@gmail.com>
2020-08-30 00:35:07 -0700

    Update LowestCommonAncestorBTree.java

--- LowestCommonAncestorBTree.java
+++ LowestCommonAncestorBTree.java
@@ -7,6 +7,8 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+//TC: O(n), n: number of nodes in the tree
+//SC: O(n)
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         List<TreeNode> path1 = new ArrayList();
