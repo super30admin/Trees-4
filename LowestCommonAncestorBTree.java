146e895ce88481ee604fb33353fe88291524e1be
Arpit Sharma <arpit317@gmail.com>
2020-08-30 00:34:09 -0700

    Update LowestCommonAncestorBTree.java

--- LowestCommonAncestorBTree.java
+++ LowestCommonAncestorBTree.java
@@ -36,3 +36,20 @@ class Solution {
         path.remove(path.size()-1); //backtrack
     }
 }
+
+//TC: O(n), n: number of nodes in the tree
+//SC: O(n)
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //base
+        if(root == null || root == p || root == q) return root;
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if(left!=null && right != null) return root;
+        if(left!= null) return left;
+        if(right!=null) return right;
+        return null;
+    }
+}
