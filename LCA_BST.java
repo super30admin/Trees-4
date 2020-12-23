9339fb2deba366d4ec5f44734728869f2d6f4f0b
pradumna123 <ps6275@gmail.com>
2020-12-22 19:17:00 -0500

    Create LCA_BST.java

---
+++ LCA_BST.java
@@ -0,0 +1,16 @@
+class Solution {
+//o(n) spc and tc
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        
+        while (root!=null){
+            if(p.val<root.val&& q.val<root.val) root=root.left;
+            
+            else if(p.val>root.val&& q.val>root.val) root=root.right;
+            
+            else return root;
+        }
+        return null;
+        
+    }
+}
