a2674b5c54e0fad077452e6dcc87e30fd3cf63f4
Rishi Raj <rishiraj824@gmail.com>
2021-01-08 22:33:01 -0500

    Update lca-binary.java

--- lca-binary.java
+++ lca-binary.java
@@ -1,3 +1,30 @@
+// Recursive
+// Time - O(N)
+// Space - O(N)
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+         if(root == null || root == p || root == q) {
+             return root;
+         }
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null) {
+            return root;
+        }
+        else if(left != null) {
+            return left;
+        }
+        else if(right != null) {
+            return right;
+        }
+        else {
+            return null;
+        }
+    }
+}
+
 // Time - O(N)
 // Space - O(N)
 class Solution {
