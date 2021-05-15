6292bc46badf4df60857b2d3a4a962e493c84bbc
Yash Shah <yash2396@hotmail.com>
2021-05-15 16:07:27 -0700

    Completed Sprint Trees-4

---
+++ LCA_BT.java
@@ -0,0 +1,29 @@
+// Time Complexity : O(N)
+// Space Complexity : O(H)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left!= null && right != null) return root;
+        if(left == null) return right;
+        if(right == null) return left;
+        return null;
+        
+        
+    }
+}
