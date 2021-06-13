fb0fa450e48e4f8caa97de48e24bc3530d32977f
Shrinivas Miskin <64209820+Shrinivasmiskin@users.noreply.github.com>
2021-06-13 19:03:47 +0530

    Kth Smallest Element in a BST
    Time Complexity = O(k)
    Space Complexity = O(k)

---
+++ KthSmallestElementBST.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> st = new Stack<>();
+        while(root != null || !st.isEmpty()){
+            while(root != null){
+                st.push(root);
+                root = root.left; 
+            }
+            root = st.pop();
+            k--;
+            if(k == 0) return root.val;
+            root = root.right; 
+        }
+    return -1;
+    }
+}
