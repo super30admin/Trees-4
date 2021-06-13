cccf8cf36dce57f9534774fde957c75a287e444c
Shrinivas Miskin <64209820+Shrinivasmiskin@users.noreply.github.com>
2021-06-13 19:12:18 +0530

    Update KthSmallestElementBST.java

--- KthSmallestElementBST.java
+++ KthSmallestElementBST.java
@@ -29,3 +29,36 @@ class Solution {
     return -1;
     }
 }
+=========================================================================================
+    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result;
+    int count;
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root, k);
+        return result;
+    }
+    
+    private void helper(TreeNode root, int k){
+        if(root == null) return;
+        helper(root.left, k);
+        count++;
+        if(k == count) result = root.val;
+        helper(root.right, k);
+    }
+}
