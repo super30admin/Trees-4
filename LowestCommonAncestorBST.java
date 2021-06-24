d4344e4895e6aefae5f4ba66f786597886a8cd1d
Kunj-97 <57332203+Kunj-97@users.noreply.github.com>
2021-06-23 23:34:22 -0400

    Update LowestCommonAncestorBST.java

--- LowestCommonAncestorBST.java
+++ LowestCommonAncestorBST.java
@@ -1,32 +1,4 @@
-/**
-================================= Recursive approach ==========================================
-Time: O(H) 
-Space: O(H) stack space 
-Approach:
-========
-1. Use technique that LCA would be in the middle of p and q value-wise
-2. If one of them is smaller, the other is larger, return root
-3. If both are larger than root, search in right subtree
-4. If both are smaller than root, search in left subtree
- */
-class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
-        if(root == null) 
-            return root;
-        
-        if(p.val < root.val && q.val < root.val)            // both are lower than root value, so search in left subtree
-            return lowestCommonAncestor(root.left, p , q);
-        else if(p.val > root.val && q.val > root.val)       // both are larger than root value, so search in right subtree
-            return lowestCommonAncestor(root.right, p, q);
-        else
-            return root;                        // found LCA
-    }
-}
 
-
-/**
-================================= Iterative approach ==========================================
 Time: O(H) 
 Space: O(1) space 
 Approach:
