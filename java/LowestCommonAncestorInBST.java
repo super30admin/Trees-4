9a32769a0ac4a71ec50f3a7b9f1fc07a97d27b93
jisakp7 <kavya.pathi@jbhunt.com>
2021-08-12 21:28:33 -0500

    LowestCommonAncestorInBST.java approach2

--- java/LowestCommonAncestorInBST.java
+++ java/LowestCommonAncestorInBST.java
@@ -37,3 +37,44 @@ class Solution {
         }
     }
 }
+Approach 2------------------------
+// Time Complexity : O(H)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null) {
+            return null;
+        }
+
+        while (root != null) {
+
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+
+                return root;
+            }
+        }
+        return null;
+
+    }
+}
