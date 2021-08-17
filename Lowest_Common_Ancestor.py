c919a20d670bf3d6a39cd1c58b3b878c0630a12d
mahavird <mahaviredx@gmail.com>
2021-08-16 21:39:59 -0400

    Trees4

---
+++ Lowest_Common_Ancestor.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#time:O(log)
+#space:O(1)
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        while(root):
+            if root.val<p.val and root.val<q.val:
+                root = root.right
+            elif root.val>p.val and root.val>q.val:
+                root = root.left
+            else:
+                return root
---
+++ Lowest_Common_Ancestor_Binary_Tree.py
@@ -0,0 +1,16 @@
+#time:O(n)
+#Space:O(logn)
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left and not right:
+            return None
+        if not right and left:
+            return left
+        if not left and right:
+            return right
+        if right and left:
+            return root
