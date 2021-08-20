b4597a43eff89f85f80d99b0ee68627efcbbf117
Radhika Kulkarni <radhikankulkarni@gmail.com>
2021-08-19 17:57:12 -0700

    Completed LCA of Binary Tree and Binary Search Tree

---
+++ LCAOfBT.java
@@ -0,0 +1,21 @@
+// TC: O(n)
+// SC: O(n)
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return null;
+        }
+        if(root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null) {
+            return root;
+        } else if(left != null) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+}
