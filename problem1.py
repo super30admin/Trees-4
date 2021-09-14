139eae5550a3996356895cd2e27ff1b5947d5272
Yash Naredi <yashnaredi26@gmail.com>
2021-09-14 12:04:05 -0400

    completed trees-4

---
+++ problem1.py
@@ -0,0 +1,21 @@
+#KTH SMALLEST ELEMENT IN A BINARY TREE
+#TC: O(H)
+#SC: O(N)
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
