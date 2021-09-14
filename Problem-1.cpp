311124ad018e1df8446988d088faef3b3f7cd3bc
Durgesh Patil <patildurgesh1998@gmail.com>
2021-09-14 14:30:21 -0400

    Create Problem-1.cpp

---
+++ Problem-1.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ Time Complexity = O(h+n)
+ Space Complexity = O(h) 
+ */
+class Solution {
+public:
+    int ans=0;
+    int count;
+    void dfs(TreeNode* root, int k){
+        //base
+        if(root==NULL)
+            return;
+        //logic
+        dfs(root->left,k);
+        count++;
+        if(count==k)
+            ans = root->val;
+        if(!ans)
+            dfs(root->right,k);    
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        count=0;
+        dfs(root,k);
+        return ans;
+    }
+};
