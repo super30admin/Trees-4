fd528d21a914eae6b3bd397fa901970a5359afcd
Durgesh Patil <patildurgesh1998@gmail.com>
2021-09-14 14:46:19 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -10,6 +10,7 @@
  * };
  Time Complexity = O(h+n)
  Space Complexity = O(h) 
+ where h is the height of the tree and n is the number of nodes in the tree.
  */
 class Solution {
 public:
@@ -30,6 +31,42 @@ public:
     int kthSmallest(TreeNode* root, int k) {
         count=0;
         dfs(root,k);
+        return ans;
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ Time Complexity = O(h+n)
+ Space Complexity = O(h) 
+ where h is the height of the tree and n is the number of nodes in the tree.
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> s;
+        int ans;
+        while(root!=NULL || !s.empty()){
+            while(root!=NULL){
+                s.push(root);
+                root=root->left;
+            }
+            k--;
+            root = s.top();
+            s.pop();
+            if(k==0)
+                return root->val;
+            root=root->right;
+        }
         return ans;
     }
 };
