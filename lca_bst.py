fb79eaca581f2eafc7836f43678796f89697fcce
meghasn <41339621+meghasn@users.noreply.github.com>
2021-10-10 02:05:28 +0530

    did lca_bst

---
+++ lca_bst.py
@@ -0,0 +1,26 @@
+# // Time Complexity :O(logn)
+# // Space Complexity :O(1)
+# // Did this code successfully run on Leetcode :yes
+# // Any problem you faced while coding this :no
+
+
+# // Your code here along with comments explaining your approach
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while True:
+            if root.val<p.val and root.val<q.val:
+                root=root.right
+            elif root.val>p.val and root.val>q.val:
+                root=root.left
+            else:
+                return root
+        
