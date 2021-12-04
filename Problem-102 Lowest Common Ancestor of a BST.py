34487d9598a28b034ab1fb0c12f439be3c95aab7
Adhish Shrivastava <adhish70@gmail.com>
2021-12-04 15:38:36 -0500

    Initial Commit

---
+++ Problem-102 Lowest Common Ancestor of a BST.py
@@ -0,0 +1,18 @@
+# 235. Lowest Common Ancestor of a Binary Search Tree
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:         
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        pass
---
+++ Problem-103 Lowest Common Ancestor of a BT.py
@@ -0,0 +1,18 @@
+# 236. Lowest Common Ancestor of a Binary Tree
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        pass
