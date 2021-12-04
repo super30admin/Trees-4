30bd1c939d09545d418660bb35c1d590f89ddd28
Adhish Shrivastava <adhish70@gmail.com>
2021-12-04 16:02:25 -0500

    Problem102 done

--- Problem-102 Lowest Common Ancestor of a BST.py
+++ Problem-102 Lowest Common Ancestor of a BST.py
@@ -1,10 +1,15 @@
 # 235. Lowest Common Ancestor of a Binary Search Tree
 # https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 
-# Logic: 
+# Logic: We use the property of a BST that the left child is less than node and 
+# right child is more than the root. There are 3 cases possible, p and q lie on 
+# the left, they lie on the right or they lie on either side. If there are not 
+# on the same side, then we know the current node is the LCA. If they both lie 
+# on the left, we more the current node towards the left and same for right. 
+# Then we again check root value with p and q.
 
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(h)
+# Space Complexity: O(h)
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -15,4 +20,9 @@
 
 class Solution:         
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pass
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
