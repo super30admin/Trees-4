d1ede89141d83fffd0a3e2e159704dd5214fb0ba
Salman Karim <salmankarimzindani@hotmail.com>
2022-01-19 08:44:05 -0600

    [ancestorNode] adds optimized approach

--- ancestorNode.js
+++ ancestorNode.js
@@ -1,10 +1,37 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity : O(n)
+// Space Complexity :O(h)
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : no
 
+// Optimized approach:
+var lowestCommonAncestor = function (root, p, q) {
+  if (!root) return null;
+  let ancestralNode;
+  const dfs = (root) => {
+    // base condition
+    if (
+      (root.val <= p.val && root.val >= q.val) ||
+      (root.val >= p.val && root.val <= q.val)
+    ) {
+      ancestralNode = root;
+      return root;
+    }
+
+    // action
+    if (root && root.val > p.val && root.val > q.val) {
+      return dfs(root.left);
+    }
+    if (root && root.val < p.val && root.val < q.val) {
+      return dfs(root.right);
+    }
+  };
+
+  dfs(root);
+  return ancestralNode;
+};
+
 // Time Complexity : O(n + n): first n is for traversing the tree, and the second is for looping through holdingArr.
-// Space Complexity : O(h)
+// Space Complexity : O(h + h): first h is for recursive stack and second h is for items in holdingArr.
 var lowestCommonAncestor = function (root, p, q) {
   if (!root) return null;
   const holdingArr = [];
