c7da969240fac2700086e4d20c8025a896bdcaa1
Salman Karim <salmankarimzindani@hotmail.com>
2022-01-20 11:59:16 -0600

    adds further optimization

--- kThSmallestInaBst.js
+++ kThSmallestInaBst.js
@@ -1,7 +1,46 @@
 // Time Complexity : O(n)
-// Space Complexity : O(n)
+// Space Complexity : O(h)
 // Did this code successfully run on Leetcode : yes
 
+// Further optimization by not traversing the right sub tree if k === 0
+var kthSmallest = function (root, k) {
+  let kthElement = 0;
+  const inorderTrav = (root) => {
+    // base
+    if (!root) return;
+
+    // Logic
+    inorderTrav(root.left);
+    k--;
+    if (!k) return (kthElement = root.val);
+    if (k > 0) inorderTrav(root.right);
+  };
+
+  inorderTrav(root);
+  return kthElement;
+};
+
+// Time Complexity : O(n)
+// Space Complexity : O(h)
+var kthSmallest = function (root, k) {
+  let kthElement = 0;
+  const inorderTrav = (root) => {
+    // base
+    if (!root) return;
+
+    // Logic
+    inorderTrav(root.left);
+    k--;
+    if (!k) return (kthElement = root.val);
+    inorderTrav(root.right);
+  };
+
+  inorderTrav(root);
+  return kthElement;
+};
+
+// Time Complexity : O(n)
+// Space Complexity : O(h + n): recursive stack and array space
 var kthSmallest = function (root, k) {
   const resultsArr = [];
   const inorderTrav = (root) => {
@@ -17,3 +56,18 @@ var kthSmallest = function (root, k) {
   inorderTrav(root);
   return resultsArr[k - 1];
 };
+
+// var kthSmallest = function(root, k) {
+//   const inorderTrav = (root) => {
+//       // base
+//       if (!root) return;
+
+//       // Logic
+//       inorderTrav(root.left)
+//       k--
+//       if (!k) return kthElement = root.val
+//       inorderTrav(root.right)
+//   }
+
+//   return inorderTrav(root);
+// };
