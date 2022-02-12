7983ed8cfa522913511ce52e639329ff660106f5
harshaanirudh <harsha.anirudh@gmail.com>
2022-02-11 16:16:47 -0800

    Update LCABST.java

--- LCABST.java
+++ LCABST.java
@@ -20,3 +20,27 @@ class Solution {
             return root;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(true){
+        if(p.val>root.val && q.val>root.val)
+            root= root.right;
+        else if(p.val<root.val && q.val<root.val)
+            root= root.left;
+        else 
+            return root;
+    }
+    }
+}
