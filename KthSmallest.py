948092e5f531a14762834204a64e4c1b875c4830
SaiAbhiramAtluri <94428363+SaiAbhiramAtluri@users.noreply.github.com>
2022-02-16 09:57:10 -0800

    Completed Trees-4

---
+++ KthSmallest.py
@@ -0,0 +1,28 @@
+# Time Complexity (o(n))
+# Space Complexity (o(n))
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+            
+        res = []
+        
+        def helper(root):
+            
+            if root == None:
+                return
+            helper(root.left)
+            
+            res.append(root.val)
+            
+            helper(root.right)
+            
+        helper(root)
+        
+                
+        return res[ k -1]
+                
