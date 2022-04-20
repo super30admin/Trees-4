b1ca1d0dd482884e7affeac85f7ac8c47a227128
Hitesh Purshotam Dayaramani <hdayaram@tibco.com>
2022-04-20 19:12:10 +0530

    Completed Trees-4

---
+++ LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,17 @@
+//Time Complexity O(N)
+//Space Complexity O(log n)
+//LeetCode tested
+
+public class LowestCommonAncestorOfABinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(root.val == p.val || root.val == q.val){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left != null && right != null) return root;
+        else if (left == null) return right;
+        else return left;
+    }
+}
