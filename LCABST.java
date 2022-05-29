4d5c83fb843af4971ad7ca96059c8d2b338f330c
surajbodapati <lbodapat@buffalo.edu>
2022-05-28 21:15:53 -0400

    4.1

--- LCABST.java
+++ LCABST.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(N) where N is the number of nodes
+// Space Complexity : O(h), h is height of tree
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+// Check left side of tree and right side, if both present then root is the ancestor
 public class LCABST {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null) return null;
