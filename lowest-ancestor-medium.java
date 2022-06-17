4a21ab881ff14017718184749b7062f8784694e1
karthik <shilpaparameshwara1997@gmail.com>
2022-06-17 15:09:28 +0530

    done

---
+++ lowest-ancestor-medium.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+            return null;
+        }
+        if(p.val == q.val){
+            return null;
+        }
+        if ((p.val == root.val) || (q.val == root.val)){
+            return root;
+        }if(((p.val < root.val) && (q.val > root.val)) || ((q.val < root.val) && (p.val > root.val))){
+            return root;
+        }if(p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }else{
+            return lowestCommonAncestor(root.right,p,q);
+        }
+
+    }
+}
+
