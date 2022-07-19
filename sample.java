c6993eb45a0026e77525add3dd027c591514ee48
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-07-19 09:00:59 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,33 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
 
+*****102.235.Lowest Common anchestor in Binary search tree****
+// Time Complexity :0(h);
+// Space Complexity :0(1);
+// Did this code successfully run on Leetcode :y
+// Any problem you faced while coding this :n
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 
-// Your code here along with comments explaining your approach
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p.val<root.val && q.val<root.val) 
+        {
+            return  lowestCommonAncestor(root.left, p, q);
+        }
+        else if(p.val>root.val && q.val>root.val)
+        {
+            return  lowestCommonAncestor(root.right, p, q);
+        }
+        else 
+        {
+            return root;
+        }
+        
+    }
+}
