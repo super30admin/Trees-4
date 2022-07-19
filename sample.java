7526d2a39e7a947575efc8e176de3470a9863575
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-07-19 10:44:08 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -103,3 +103,52 @@ class Solution {
         path.remove(path.size()-1);
     }
 }
+//****Kth SMALLEST ELEMENT IN BST****
+//Time complexity:o(n);
+//Space complexity: 0(1);
+//Leetcode runnable: Y;
+//Any doubts: N;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count;
+    int result;
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return result;
+        
+    }
+    
+    public void inorder(TreeNode root, int k)
+    {
+        //Base
+        if(root==null)
+        {
+            return;
+        }
+        
+        //logic
+        inorder(root.left,k);
+        count++;
+        if(count==k)
+        {
+            result=root.val;
+        }
+        inorder(root.right, k);
+        
+    }
+}
