8c4bfcbc22aab86f49cd85b16436186abf4441c3
akshaykmus <akshaykmus@gmail.com>
2022-07-21 20:13:11 -0400

    Completed LCA problems.

---
+++ LCAofBST.java
@@ -0,0 +1,12 @@
+//Time : O(h)
+//Space :O(1)
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val)
+            return lowestCommonAncestor(root.left, p, q);
+        else if (p.val > root.val && q.val > root.val)
+            return lowestCommonAncestor(root.right, p, q);
+        else
+            return root;
+    }
+}
