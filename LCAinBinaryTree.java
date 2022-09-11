bc0821ddf29a1b88d5c9fe1ca57f616babd98295
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-10 17:20:41 -0700

    Update LCAinBinaryTree.java

--- LCAinBinaryTree.java
+++ LCAinBinaryTree.java
@@ -51,3 +51,58 @@ class Solution {
     
 }
 
+
+
+class Solution {
+    List<TreeNode> pathP;
+    List<TreeNode> pathQ;
+    boolean flagP;
+    boolean flagQ;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        helper(root, p, q, new ArrayList<>());
+      
+        for(int i = 0; i < pathP.size(); i++){
+            if(pathP.get(i) != pathQ.get(i)){
+                return pathP.get(i-1); // last before value
+            }
+        }
+        return null;
+    }
+   
+    private void helper(TreeNode root, TreeNode p, TreeNode q, List<TreeNode> path){
+        //base
+        if(root == null) return;
+
+        //logic  
+        //Backtracking
+        //Action
+        path.add(root);
+        
+         if(root == p){
+            flagP = true;
+            pathP = new ArrayList<>(path);
+            pathP.add(root);
+        }
+        if(root == q){
+            flagQ = true;
+            pathQ = new ArrayList<>(path);
+            pathQ.add(root);
+        }
+        
+        //If both are found on left or right no need to go other side
+        //Recurse
+        if(!flagP || !flagQ)
+            helper(root.left,  p, q, path);
+        //st pop
+        if(!flagP || !flagQ)
+          helper(root.right, p, q, path);
+        //st pop
+        
+        //BackTrack
+        path.remove(path.size()-1);
+  
+    }
+    
+}
+
