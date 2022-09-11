b300cf593ba2ab7231f8bfd89ac83911d1e4ed67
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-09-10 17:49:47 -0700

    Update LCAinBinaryTree.java

--- LCAinBinaryTree.java
+++ LCAinBinaryTree.java
@@ -1,3 +1,25 @@
+//Important Algorithm ****
+//Approach -  3 : Recursion
+//Time Complexity : O(n)
+//Space Complexity : O(h)
+
+class Solution {
+   
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+  
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left,  p,  q);
+        TreeNode right = lowestCommonAncestor(root.right,  p,  q);
+        
+        if(left == null && right == null) return null; // not found
+        else if(left == null && right != null) return right; //left side
+        else if(left != null && right == null) return left; // right side 
+        else return root; // p, q are both sides
+
+    }
+}
+
+
 //Approach - 1 : Backtracking
 //Time Complexity : O(n)
 //Space Complexity : O(h)
