39db61285acb529ee671a7ba4ac66f6d6a16c15f
“DhirenP93” <dparyani93@gmail.com>
2022-09-11 22:08:15 -0700

    Completed Trees-4

--- KthSmallestElement.java
+++ KthSmallestElement.java
@@ -3,6 +3,7 @@ class Solution {
     //Inorder traversal in a BST gives elements in sorted order
     //We can maintain a count and whenever it becomes equal to k we return
     // Why we have count as global because if we send count as an argument it would be become local to the recursion
+    //Optimization: Condition Recursion: Only Recur if element is not yet found
 
     int count=0;
     int rootVal=Integer.MIN_VALUE;
