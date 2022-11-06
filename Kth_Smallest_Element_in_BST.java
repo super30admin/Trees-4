cf141e3733f1333881540bf47c76eb7094baa858
dileepdomakonda <40898595+dileepdomakonda@users.noreply.github.com>
2022-11-06 11:03:36 -0600

    Trees-4_HW

---
+++ Kth_Smallest_Element_in_BST.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// TC : O(k)
+// SC : O(h)
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        
+        Stack<TreeNode> st = new Stack<>();
+        
+        while(root != null || !st.isEmpty()) {
+            
+            while(root != null) {
+                st.push(root);
+                root = root.left;
+            }
+            k--;
+            root = st.pop();
+            if(k == 0)
+                return root.val;
+            root = root.right;
+        }
+        return -1;
+    }
+}
