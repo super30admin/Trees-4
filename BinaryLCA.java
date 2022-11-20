f6c87ad94d3d62229b76fa356a845b29eb5a5c05
Shivani31996 <shivani.chavan96@gmail.com>
2022-11-20 17:00:39 -0500

    Please review

--- BinaryLCA.java
+++ BinaryLCA.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // Time Complexity : O(n)
 // Space Complexity :O(h)
 // Did this code successfully run on Leetcode : Yes
@@ -58,3 +61,36 @@ public class BinaryLCA {
         path.remove(path.size() - 1);
     }
 }
+
+/*
+ * Approach 2
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root == null || root == p || root == q)
+            return root;
+        
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        
+        if(left == null && right == null)
+            return null;
+        else if(left != null && right == null)
+            return left;
+        else if(left == null && right != null)
+            return right;
+        else
+            return root;
+    }
+}
+
