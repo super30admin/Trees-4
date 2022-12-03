a6ca28ec5fddcbd60fef70f4fb56d08fd53d4f1f
eshwar2807 <114554105+eshwar2807@users.noreply.github.com>
2022-12-03 15:17:13 -0800

    Create Exercise_2.java

---
+++ Exercise_2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root ==q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left!= null && right!=null){
+            return root;
+        }
+
+        return left!=null ? left : right;
+    }
+}
+//tc = O(nh)
+//sc = O(1)
