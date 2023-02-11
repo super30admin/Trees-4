a050f351acff7dd31da903fd37711cb97b740cd0
Rohit Bassi <rohitbassi@Rohits-MacBook-Pro.local>
2023-02-11 10:51:19 -0800

    completed trrees 4

---
+++ problem1.py
@@ -0,0 +1,15 @@
+#time O(N)
+#space O(N)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        def inorder(r):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+    
+        return inorder(root)[k-1]
