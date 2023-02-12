5150c66869534a1efc1bfe74515fc83e201837da
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-02-11 21:53:30 -0500

    Create main.go

---
+++ kthSmallestInBST/main.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// time: o(k) or worst case; o(n)
+// space: o(h) or worst case o(n)
+func kthSmallest(root *TreeNode, k int) int {
+    var dfs func(r *TreeNode) *TreeNode
+    dfs = func(r *TreeNode) *TreeNode {
+        // base
+        if r == nil {return nil}
+        
+        
+        // logic
+        left := dfs(r.Left)
+        if left != nil {return left}
+        
+        k--
+        if k == 0 {return r}
+        
+        return dfs(r.Right)
+    }
+    return dfs(root).Val
+}
