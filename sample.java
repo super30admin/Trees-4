968b6bf1e825a23ede51887a3b0cd4facff54278
MalavyaRaval <malavyaraval@gmail.com>
2023-03-22 16:15:49 -0700

    Completed

--- sample.java
+++ sample.java
@@ -1,7 +1,37 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(H)
+// Space Complexity :O(H+k)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> st = new Stack<>();
+        while(true){
+            while(root!= null){
+                st.push(root);
+                root = root.left;
+            }
+            root= st.pop();
+            k--;
+            if(k==0) return root.val;
+            root = root.right;
+        }
+    }
+}
