8b522e8de756f50c38853fa6d322f91eafefccff
sivakami-projects <126191544+sivakami-projects@users.noreply.github.com>
2023-04-19 17:31:44 -0700

    Create KthSmallestBST.java

---
+++ KthSmallestBST.java
@@ -0,0 +1,19 @@
+/*
+    Time Complexity - O(H + k), H is a tree height. 
+    Space Complexity - O(H).
+ */
+class Solution {
+  public int kthSmallest(TreeNode root, int k) {
+    LinkedList<TreeNode> stack = new LinkedList<>();
+
+    while (true) {
+      while (root != null) {
+        stack.push(root);
+        root = root.left;
+      }
+      root = stack.pop();
+      if (--k == 0) return root.val;
+      root = root.right;
+    }
+  }
+}
