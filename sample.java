0ca4d1fc7ebfa53397aef5eae578e28169d081c2
Pranay Joshi <pranay@Pranays-MBP.attlocal.net>
2023-06-08 04:17:09 -0500

    Completed Assignment_31 on Trees-4 Please Review

--- sample.java
+++ sample.java
@@ -1,7 +1,117 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+## Problem1 Kth Smallest Element in a BST (https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/)
 
+// Time Complexity : 0(n)
+// Space Complexity : 0(h)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
-// Your code here along with comments explaining your approach
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result;
+    int count;
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null) {
+            return -1;
+        }
+        result = 0;
+        inorder(root, k);
+        return result;
+    }
+    private void inorder(TreeNode root, int k) {
+        if (root == null || result != 0) {
+            return;
+        }
+        inorder(root.left, k);
+        count++;
+        if (count == k) {
+            result = root.val;
+            return;
+        }
+        inorder(root.right, k);
+    }
+}
+
+## Problem2 Lowest Common Ancestor of a Binary Search Tree (https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/)
+
+// Time Complexity : 0(logn)
+// Space Complexity : 0(logn)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null) {
+            return null;
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
+
+## Problem3 Lowest Common Ancestor of a Binary Tree (https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/)
+
+// Time Complexity : 0(n)
+// Space Complexity : 0(h)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null && right == null) {
+            return null;
+        }
+        else if(left == null && right != null) {
+            return right;
+        }
+        else if(left != null && right == null) {
+            return left;
+        }
+        else {
+            return root;
+        }
+        
+    }
+}
