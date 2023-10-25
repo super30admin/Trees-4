b186ffc2c3bb5b9e4a0de4a4c95d962d7c58a655
Shridhar Bhardwaj <57531390+shridharbhardwaj@users.noreply.github.com>
2023-10-24 22:38:45 -0700

    Completed Trees-4

---
+++ ksmallest.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Time complexity = O(N)
+# Space complexity = O(1)
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+
+            root = stack.pop()
+            k -= 1
+
+            if not k:
+                return root.val
+
+            root = root.right
